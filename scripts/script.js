e399f20b9c4a2467d206b736777de962f0faf0d5
Daniel Velez <velezdaniel516@gmail.com>
2026-01-05 12:27:11 -0500

    Add make grid function for div generation

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+//Create make grid function
+function makeGrid(gridSize) {
+    //Select screen element
+    const sketchScreen = document.getElementById('sketch-screen')
+
+    //Create for loop on column generation
+    for(let i = 0; i < gridSize; i++) {
+        //Create div column
+        const column = document.createElement('div');
+        //Add column class to div
+        column.classList.add("column");
+        //Create for loop on row generation
+        for(let j = 0; j < gridSize; j++) {
+            //Create grid row div
+            const row = document.createElement('div');
+            //row class to div
+            row.classList.add("row");
+            //Add border styling to row
+            row.style.border = "2px solid black";
+            row.innerText = (i * gridSize) + j;
+            column.appendChild(row)
+        }
+        sketchScreen.appendChild(column);
+    }
+}
+
+//Call make grid function
+makeGrid(16);
+
