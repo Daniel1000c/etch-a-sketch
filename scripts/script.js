fab59b5641ee8f30f924b13b01676f8edad79b0b
Daniel Velez <velezdaniel516@gmail.com>
2026-01-08 12:58:50 -0500

    Add screen and row consts

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+//Create const for screen size, number of squares for each side
+const SCREEN_SIZE = 600;
+let numberOfSquares = 16;
+
+//Create screen element
+const screen = document.getElementById('sketch-screen');
+screen.style.width = screen.style.height = `${SCREEN_SIZE}px`;
